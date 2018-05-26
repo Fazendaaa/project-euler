e3c22f358e09f7ca7fc0cd2d959f5f4ac8ee9f62
farm <lukas_karotta@hotmail.com>
2018-05-25 22:41:28 -0300

    Finished problem 10.

--- src/haskell/1-25/problem_10.hs
+++ src/haskell/1-25/problem_10.hs
@@ -9,13 +9,19 @@
 import Data.List
 import Data.List.Unique
 
+myDiff :: Integral a => a -> [a] -> [a]
+myDiff _ [] = []
+myDiff n (x:xs)
+    | (==) n x = myDiff ((+) x 1) xs
+    | otherwise = (++) [n..((-) x 1)] $ myDiff ((+) x 1) xs
+
 erasthotenesSieve :: Integral a => a -> [a]
 erasthotenesSieve limit = primes where
     multiplesLimit = (+) 1 $ truncate ((sqrt $ fromIntegral limit):: Float)
     multiples = sortUniq [ mult | x <- [2..multiplesLimit], mult <- takeWhile (<= limit) [ ((*) x y) | y <- [2..limit] ]]
-    primes = (\\) [2..limit] multiples
+    primes = myDiff 2 multiples
 
 main :: IO()
 main = do
     let limit = 2000000
-    print $ erasthotenesSieve limit
+    print $ foldl' (+) 0 $ erasthotenesSieve limit
