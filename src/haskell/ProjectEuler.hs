1a4a7a8d6a2542c2fccf1c2da2008ad9d0172455
farm <lukas_karotta@hotmail.com>
2018-05-28 01:43:13 -0300

    Finished problem 11.

--- src/haskell/ProjectEuler.hs
+++ src/haskell/ProjectEuler.hs
@@ -1,4 +1,13 @@
 module ProjectEuler where
+    import Data.List
+    import Data.List.Unique
+
+    stringToNumbers :: String -> [Int]
+    stringToNumbers [] = []
+    stringToNumbers (x:xs)
+        | (/=) '\n' x = (digitToInt x) : (stringToNumbers xs)
+        | otherwise = (stringToNumbers xs)
+
     isPrime :: Integral a => a -> Bool
     isPrime number
         | (==) 2 number = True
@@ -6,8 +15,14 @@ module ProjectEuler where
         | otherwise = False where
             multiplesLimit = (+) 1 $ truncate ((sqrt $ fromIntegral number):: Float)
 
+    myDiff :: Integral a => a -> [a] -> [a]
+    myDiff _ [] = []
+    myDiff n (x:xs)
+        | (==) n x = myDiff ((+) x 1) xs
+        | otherwise = (++) [n..((-) x 1)] $ myDiff ((+) x 1) xs
+
     erasthotenesSieve :: Integral a => a -> [a]
     erasthotenesSieve limit = primes where
         multiplesLimit = (+) 1 $ truncate ((sqrt $ fromIntegral limit):: Float)
-        multiples = [ y*x | x <- [2..multiplesLimit], y <- [2..multiplesLimit] ]
-        primes = filter (\num -> not $ elem num multiples ) [2..limit]
+        multiples = sortUniq [ mult | x <- [2..multiplesLimit], mult <- takeWhile (<= limit) [ ((*) x y) | y <- [2..limit] ]]
+        primes = myDiff 2 multiples
