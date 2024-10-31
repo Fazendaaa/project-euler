65aa47f4c6cf920ada04d2fc41c0888ae0433a12
Faendaaa <lucas.carotta@outlook.com>
2024-10-31 14:41:49 -0300

    Adding performance graph generator

--- src/r/performance/problem-5.R
+++ src/r/performance/problem-5.R
@@ -1,22 +1,40 @@
+#!/bin/Rscript
 # Rscript performance/problem-5.R
-library(profvis)
-library(htmlwidgets)
-library(testthat)
 library(projectEuler)
+library(ggplot2)
+library(plotly)
+library(htmlwidgets)
+library(reshape2)
 
-example <- profvis({
+example <- profvis::profvis({
   input <- 10
   output <- 2520
 
-  expect_equal(problem5(input), output)
+  testthat::expect_equal(problem5(input), output)
 })
 
-wanted <- profvis({
+wanted <- profvis::profvis({
   input <- 20
   output <- 232792560
 
-  expect_equal(problem5(input), output)
+  testthat::expect_equal(problem5(input), output)
 })
 
-saveWidget(example, 'inst/data/example.html')
-saveWidget(wanted, 'inst/data/wanted.html')
+toMaintain <- c('time', 'memalloc')
+
+dfExample <- example[['x']][['message']][['prof']]
+dfWanted <- wanted[['x']][['message']][['prof']]
+dfExample <- tail(dfExample[toMaintain], n = 1)
+dfWanted <- tail(dfWanted[toMaintain], n = 1)
+
+dfExample[['implementation']] <- c('example')
+dfWanted[['implementation']] <- c('wanted')
+
+merged <- merge(dfExample, dfWanted, all = TRUE)
+write.table(merged, 'inst/merged.csv', sep = ',')
+
+data <- melt(read.csv('inst/merged.csv'), id.vars = 'implementation')
+plot <- ggplot(data, aes(x = implementation, y = value, fill = variable)) +
+  geom_bar(stat = 'identity') +
+  facet_wrap(~ variable)
+saveWidget(ggplotly(plot), 'inst/merged.html', selfcontained = TRUE)
