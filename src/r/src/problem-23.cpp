1992b325c173dd2969b7a6e23820e35033c0c6ca
Faendaaa <lucas.carotta@outlook.com>
2024-11-17 19:23:31 -0300

    Improving implementation

--- src/r/src/problem-23.cpp
+++ src/r/src/problem-23.cpp
@@ -1,18 +1,29 @@
 #include <Rcpp.h>
+#include <algorithm>
 
 using namespace Rcpp;
 
 const std::list<int> getDivisors(int number) {
-  int limit = number/2;
+  int limit = floor(sqrt(number)), tmp;
   std::list<int> divisors = {1};
 
-  for (int index = 2; index <= limit; index += 1) {
+  if (1 == number) {
+    return divisors;
+  }
+
+  for (int index = 2; index < limit; index += 1) {
     if (0 == number % index) {
       divisors.push_back(index);
+
+      tmp = floor(number / index);
+
+      if (tmp != index) {
+        divisors.push_back(tmp);
+      }
     }
   }
 
-  divisors.push_back(number);
+  divisors.sort();
 
   return divisors;
 }
@@ -20,8 +31,6 @@ const std::list<int> getDivisors(int number) {
 const bool isAbundant(int number) {
   int sum = 0;
   std::list<int> elements = getDivisors(number);
-
-  elements.pop_back();
 
   for (auto i : elements) {
     sum += i;
@@ -33,7 +42,7 @@ const bool isAbundant(int number) {
 const std::list<int> getAbundantNumbers(int limit) {
   std::list<int> abundantNumbers = {};
 
-  for (int index = 1; index <= limit; index += 1) {
+  for (int index = 1; index < limit; index += 1) {
     if (isAbundant(index)) {
       abundantNumbers.push_back(index);
     }
@@ -47,15 +56,14 @@ double problem23Cpp() {
   int limit = 28123, sum, total, start = 1;
   std::list<int> allAbundant = getAbundantNumbers(limit);
   std::list<int> sumOfTwo = {};
-  auto outerValue = allAbundant.begin(), innerValue = allAbundant.begin();
 
-  for (int outerIndex = 0; outerIndex < allAbundant.size(); outerIndex += 1) {
-    std::advance(outerValue, outerIndex);
+  for (auto i: getDivisors(100)) {
+    Rcout << i << ' ';
+  }
 
-    for (int innerIndex = outerIndex; innerIndex < allAbundant.size(); innerIndex += 1) {
-      std::advance(innerValue, outerIndex);
-
-      sum = *outerValue + *innerValue;
+  for (auto outer: allAbundant) {
+    for (auto inner: allAbundant) {
+      sum = outer + inner;
 
       if (sum > limit) {
         break;
@@ -68,12 +76,10 @@ double problem23Cpp() {
   sumOfTwo.sort();
   sumOfTwo.unique();
 
-  for (auto element: sumOfTwo) {
-    for (int index = start; index < element; index++) {
+  for (int index = 1; index <= limit; index++) {
+    if (std::find(sumOfTwo.begin(), sumOfTwo.end(), index) == sumOfTwo.end()) {
       total += index;
     }
-
-    start = element + 1;
   }
 
   return total;
