c50e9dcccd3a014ede11c6f79a162a6563111d60
Fazendaaa <lucas.carotta@outlook.com>
2025-10-14 22:26:14 -0300

    Improving exercise 11

--- src/python/src/project_euler/first/problem_11.py
+++ src/python/src/project_euler/first/problem_11.py
@@ -1,88 +1,133 @@
 """
-                            Largest product in a grid
+                        Largest product in a grid
 
-    In the 20×20 grid below, four numbers along a diagonal line have been marked
-    in red.
+In the 20×20 grid below, four numbers along a diagonal line have been marked
+in red.
 
-            08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
-            49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
-            81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
-            52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
-            22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
-            24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
-            32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
-            67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
-            24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
-            21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
-            78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
-            16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
-            86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
-            19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
-            04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
-            88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
-            04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
-            20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
-            20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
-            01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+        08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+        49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+        81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+        52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+        22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+        24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+        32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+        67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+        24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+        21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+        78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+        16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+        86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+        19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+        04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+        88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+        04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+        20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+        20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+        01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
 
-    The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
-    What  is the greatest product of four adjacent numbers in the same direction
-    ( up, down, left, right, or diagonally ) in the 20×20 grid?
-
-                        Answer: 70600674
-    helped me out:
-        *   http://stackoverflow.com/a/5878474/7092954
-        *   http://stackoverflow.com/a/6313414/7092954
+The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+What  is the greatest product of four adjacent numbers in the same direction
+                    Answer: 70600674
+helped me out:
+    *   http://stackoverflow.com/a/5878474/7092954
+    *   http://stackoverflow.com/a/6313414/7092954
 """
 
-import re
-import functools as ft
-import operator as op
-import numpy as np
+from functools import reduce
+from operator import mul
 
-def each_cons(array, limit):
-    """Given array, returns it all sets of each cons elements"""
-    return [array[i:i+limit] for i in range(len(array)-limit+1)]
+from numpy import array
 
-def greatest_product_vector(vector, limit):
-    """Given vector, returns it the max multiplied value of given limit factor"""
-    results = []
+
+def each_cons(vector: list[int], limit: int) -> list[list[int]]:
+    """
+    Returns a list of consecutive elements from the input vector.
+
+    Args:
+        vector (list[int]): Input list of integers
+        limit (int): Size of each consecutive group
+
+    Returns:
+        list[list[int]]: List of lists containing consecutive elements of size limit
+    """
+    return [vector[i : i + limit] for i in range(len(vector) - limit + 1)]
+
+
+def greatest_product_vector(vector: list[int], limit: int) -> int:
+    """
+    Finds the greatest product of consecutive elements in a vector.
+
+    Args:
+        vector (list[int]): Input list of integers
+        limit (int): Number of consecutive elements to multiply
+
+    Returns:
+        int: Maximum product found, or 0 if no products possible
+    """
+    results: list[int] = []
 
     for cons in each_cons(vector, limit):
-        results.append(ft.reduce(op.mul, cons, 1))
+        results.append(reduce(mul, cons, 1))
 
     return max(results if [] != results else [0])
 
-def greatest_product_matrix(matrix, limit):
-    """Given matrix, returns it the max multiplied value of given limit factor
-     in all of the lines"""
-    results = []
+
+def greatest_product_matrix(matrix: list[list[int]], limit: int) -> int:
+    """
+    Finds the greatest product of consecutive elements across all rows in a matrix.
+
+    Args:
+        matrix (list[list[int]]): 2D list of integers
+        limit (int): Number of consecutive elements to multiply
+
+    Returns:
+        int: Maximum product found across all rows
+    """
+    results: list[int] = []
 
     for vector in matrix:
         results.append(greatest_product_vector(vector, limit))
 
     return max(results)
 
-def greatest_product_diagonals(matrix, limit):
-    """Given matrix, returns it the max multiplied value of given limit factor
-     in all of the diagonals"""
-    matrix = np.array(matrix)
-    diags = [matrix[::-1, :].diagonal(i) for i in range(-matrix.shape[0]+1, matrix.shape[1])]
-    diags.extend(matrix.diagonal(i) for i in range(matrix.shape[1]-1, -matrix.shape[0], -1))
+
+def greatest_product_diagonals(matrix: list[list[int]], limit: int) -> int:
+    """
+    Finds the greatest product of consecutive elements along all diagonals in a matrix.
+
+    Args:
+        matrix (list[list[int]]): 2D list of integers
+        limit (int): Number of consecutive elements to multiply
+
+    Returns:
+        int: Maximum product found across all diagonals
+    """
+    matrix = array(matrix)
+    diags = [
+        matrix[::-1, :].diagonal(i)
+        for i in range(-matrix.shape[0] + 1, matrix.shape[1])
+    ]
+    diags.extend(
+        matrix.diagonal(i) for i in range(matrix.shape[1] - 1, -matrix.shape[0], -1)
+    )
     diags = [n.tolist() for n in diags]
 
     return greatest_product_matrix(diags, limit)
 
-def greatest_product(matrix, adjacent):
-    """Return all the adjacent numbers that have the greatest product"""
+
+def greatest_product(matrix: list[list[int]], adjacent: int) -> int:
+    """
+    Finds the greatest product of adjacent numbers in a matrix looking at rows, columns and diagonals.
+
+    Args:
+        matrix (list[list[int]]): 2D list of integers
+        adjacent (int): Number of adjacent elements to multiply
+
+    Returns:
+        int: Maximum product found across rows, columns and diagonals
+    """
     lines = greatest_product_matrix(matrix, adjacent)
     columns = greatest_product_matrix(list(map(list, zip(*matrix))), adjacent)
     diagonals = greatest_product_diagonals(matrix, adjacent)
 
     return max(lines, columns, diagonals)
-
-with open('./input/problem_11.txt', 'r') as file:
-    MATRIX = []
-    for line in file:
-        MATRIX.append([int(x) for x in re.findall(r'\d+', line)])
-    print(greatest_product(MATRIX, 4))
---
+++ src/python/tests/first/test_problem_11.py
@@ -0,0 +1,13 @@
+from re import findall
+
+from project_euler.first.problem_11 import greatest_product
+
+
+def test_wanted() -> None:
+    matrix: list[list[int]] = []
+
+    with open("data/input/problem_11.txt", "r") as file:
+        for line in file:
+            matrix.append([int(number) for number in findall(r"\d+", line)])
+
+    assert greatest_product(matrix, 4) == 70_600_674
